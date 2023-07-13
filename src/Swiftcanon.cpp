3860b61c534f3e054026d9862e8109e05f2b3744
rutvik <rutvik@rutviks-MacBook-Pro.local>
2023-07-13 00:32:52 -0500

    added vertex buffer to transfer vertex data instead of using hard-coded values and also implemented staging buffers to copy data faster

--- src/Swiftcanon.cpp
+++ src/Swiftcanon.cpp
@@ -66,6 +66,7 @@ void Swiftcanon::initVulkan()
     createFramebuffers();
     createCommandPool();
     createCommandBuffer();
+    createVertexBuffer();
     createSyncObjects();
 }
 
@@ -463,8 +464,8 @@ void Swiftcanon::createRenderPass()
 
 void Swiftcanon::createGraphicsPipeline()
 {
-    std::vector<char> vertShaderCode = readFile("../src/shaders/compiled/vert.spv");
-    std::vector<char> fragShaderCode = readFile("../src/shaders/compiled/frag.spv");
+    std::vector<char> vertShaderCode = readFile("src/shaders/compiled/vert.spv");
+    std::vector<char> fragShaderCode = readFile("src/shaders/compiled/frag.spv");
 
     VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
     VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
@@ -493,12 +494,14 @@ void Swiftcanon::createGraphicsPipeline()
     dynamicState.dynamicStateCount  = static_cast<uint32_t>(dynamicStates.size());
     dynamicState.pDynamicStates     = dynamicStates.data();
 
+    auto bindingDescription = Vertex::getBindingDescription();
+    auto attributeDescriptions = Vertex::getAttributeDescriptions();
     VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
     vertexInputInfo.sType                           = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
-    vertexInputInfo.vertexBindingDescriptionCount   = 0;
-    vertexInputInfo.pVertexBindingDescriptions      = nullptr; // Optional
-    vertexInputInfo.vertexAttributeDescriptionCount = 0;
-    vertexInputInfo.pVertexAttributeDescriptions    = nullptr; // Optional
+    vertexInputInfo.vertexBindingDescriptionCount   = 1;
+    vertexInputInfo.pVertexBindingDescriptions      = &bindingDescription;
+    vertexInputInfo.vertexAttributeDescriptionCount = static_cast<uint32_t>(attributeDescriptions.size());
+    vertexInputInfo.pVertexAttributeDescriptions    = attributeDescriptions.data();
 
     VkPipelineInputAssemblyStateCreateInfo inputAssembly{};
     inputAssembly.sType                             = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
@@ -662,6 +665,38 @@ void Swiftcanon::createCommandBuffer()
     }
 }
 
+void Swiftcanon::createVertexBuffer()
+{
+    VkDeviceSize bufferSize = sizeof(vertices[0]) * vertices.size();
+
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingBufferMemory;
+    createBuffer(
+        bufferSize,
+        VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+        stagingBuffer,
+        stagingBufferMemory
+    );
+
+    void* data;
+    vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+        memcpy(data, vertices.data(), (size_t) bufferSize);
+    vkUnmapMemory(device, stagingBufferMemory);
+
+    createBuffer(
+        bufferSize,
+        VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+        vertexBuffer,
+        vertexBufferMemory
+    );
+
+    copyBuffer(stagingBuffer, vertexBuffer, bufferSize);
+    vkDestroyBuffer(device, stagingBuffer, nullptr);
+    vkFreeMemory(device, stagingBufferMemory, nullptr);
+}
+
 // TODO: Massively improve scoring factors to better score the GPUs
 void Swiftcanon::ratePhysicalGraphicsDevices(VkPhysicalDevice device, int deviceIndex)
 {
@@ -789,11 +824,15 @@ void Swiftcanon::recordCommandBuffer(VkCommandBuffer command_buffer, uint32_t im
         std::cerr << string_VkResult(result) << std::endl;
         throw std::runtime_error("[VULKAN] Failed to initialize recording CommandBuffer");
     }
+
+    VkBuffer vertexBuffers[] = {vertexBuffer};
+    VkDeviceSize offsets[] = {0};
     vkCmdBeginRenderPass(command_buffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
     vkCmdBindPipeline(command_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline);
+    vkCmdBindVertexBuffers(command_buffer, 0, 1, vertexBuffers, offsets);
     vkCmdSetViewport(command_buffer, 0, 1, &viewport);
     vkCmdSetScissor(command_buffer, 0, 1, &scissor);
-    vkCmdDraw(command_buffer, 3, 1, 0, 0);
+    vkCmdDraw(command_buffer, static_cast<uint32_t>(vertices.size()), 1, 0, 0);
     vkCmdEndRenderPass(command_buffer);
     result = vkEndCommandBuffer(command_buffer);
     if (result != VK_SUCCESS) {
@@ -867,6 +906,84 @@ std::vector<char> Swiftcanon::readFile(const std::string& filename) {
     file.close();
 
     return buffer;
+}
+
+uint32_t Swiftcanon::findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties)
+{
+    VkPhysicalDeviceMemoryProperties memProperties;
+    vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+
+    for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+        if ((typeFilter & (1 << i)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
+            return i;
+        }
+    }
+    throw std::runtime_error("[Vulkan] Failed to find suitable Memory Type");
+}
+
+void Swiftcanon::createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory)
+{
+    VkBufferCreateInfo bufferInfo{};
+    bufferInfo.sType        = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+    bufferInfo.size         = size;
+    bufferInfo.usage        = usage;
+    bufferInfo.sharingMode  = VK_SHARING_MODE_EXCLUSIVE;
+
+    VkResult result = vkCreateBuffer(device, &bufferInfo, nullptr, &buffer);
+    if (result != VK_SUCCESS) {
+        std::cerr << string_VkResult(result) << std::endl;
+        throw std::runtime_error("[VULKAN] Failed to create Buffer");
+    }
+
+    VkMemoryRequirements memRequirements;
+    vkGetBufferMemoryRequirements(device, buffer, &memRequirements);
+
+    VkMemoryAllocateInfo allocInfo{};
+    allocInfo.sType             = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+    allocInfo.allocationSize    = memRequirements.size;
+    allocInfo.memoryTypeIndex   = findMemoryType(memRequirements.memoryTypeBits, properties);
+
+    result = vkAllocateMemory(device, &allocInfo, nullptr, &bufferMemory);
+    if (result != VK_SUCCESS) {
+        std::cerr << string_VkResult(result) << std::endl;
+        throw std::runtime_error("[VULKAN] Failed to allocate Buffer Memory");
+    }
+
+    vkBindBufferMemory(device, buffer, bufferMemory, 0);
+}
+
+void Swiftcanon::copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size)
+{
+    VkCommandBufferAllocateInfo allocInfo{};
+    allocInfo.sType                 = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    allocInfo.level                 = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    allocInfo.commandPool           = commandPool;
+    allocInfo.commandBufferCount    = 1;
+
+    VkCommandBuffer commandBuffer;
+    vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer);
+
+    VkCommandBufferBeginInfo beginInfo{};
+    beginInfo.sType                 = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+    beginInfo.flags                 = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+    vkBeginCommandBuffer(commandBuffer, &beginInfo);
+    
+    VkBufferCopy copyRegion{};
+    copyRegion.srcOffset            = 0; // Optional
+    copyRegion.dstOffset            = 0; // Optional
+    copyRegion.size                 = size;
+    vkCmdCopyBuffer(commandBuffer, srcBuffer, dstBuffer, 1, &copyRegion);
+
+    vkEndCommandBuffer(commandBuffer);
+
+    VkSubmitInfo submitInfo{};
+    submitInfo.sType                = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+    submitInfo.commandBufferCount   = 1;
+    submitInfo.pCommandBuffers      = &commandBuffer;
+    vkQueueSubmit(graphicsQueue, 1, &submitInfo, VK_NULL_HANDLE);
+    
+    vkQueueWaitIdle(graphicsQueue);
+    vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);
 }
 
 void Swiftcanon::mainLoop()
@@ -943,6 +1060,8 @@ void Swiftcanon::drawFrame()
 void Swiftcanon::cleanup()
 {
     cleanupSwapChain();
+    vkDestroyBuffer(device, vertexBuffer, nullptr);
+    vkFreeMemory(device, vertexBufferMemory, nullptr);
 for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
     vkDestroySemaphore(device, imageAvailableSemaphores[i], nullptr);
     vkDestroySemaphore(device, renderFinishedSemaphores[i], nullptr);
--- src/Swiftcanon.h
+++ src/Swiftcanon.h
@@ -1,9 +1,37 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
+#include <array>
 #include <vector>
 #include <string>
 #include <optional>
+
+struct Vertex {
+    glm::vec2 pos;
+    glm::vec3 color;
+    
+    static VkVertexInputBindingDescription getBindingDescription() {
+        VkVertexInputBindingDescription bindingDescription{};
+        bindingDescription.binding      = 0;
+        bindingDescription.stride       = sizeof(Vertex);
+        bindingDescription.inputRate    = VK_VERTEX_INPUT_RATE_VERTEX;
+        return bindingDescription;
+    }
+    
+    static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
+        std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
+        attributeDescriptions[0].binding    = 0;
+        attributeDescriptions[0].location   = 0;
+        attributeDescriptions[0].format     = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescriptions[0].offset     = offsetof(Vertex, pos);
+        attributeDescriptions[1].binding    = 0;
+        attributeDescriptions[1].location   = 1;
+        attributeDescriptions[1].format     = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[1].offset     = offsetof(Vertex, color);
+        return attributeDescriptions;
+    }
+};
 
 struct DeviceDetails {
     const char* name;
@@ -97,6 +125,19 @@ private:
     std::vector<VkFence>            inFlightFences;
     const int                       MAX_FRAMES_IN_FLIGHT        = 2;
     uint32_t                        currentFrame                = 0;
+
+    // Shaders Setup
+    const std::vector<Vertex>       vertices                    = {{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},{{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},{{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}};
+    VkBuffer                        vertexBuffer;
+    VkDeviceMemory                  vertexBufferMemory;
+
+    // Shaders Setup
+    void createVertexBuffer();
+
+    // Vulkan Helper Functions
+    uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties);
+    void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory);
+    void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size);
 
     // UTIL
     std::vector<char> readFile(const std::string& filename);
