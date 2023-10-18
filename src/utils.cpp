cad7057da745f3280e248ff47beaaaf0392ee5aa
rutvik <rutvik@rutviks-MacBook-Pro.local>
2023-10-17 23:21:49 -0500

    added utils namespace

--- src/utils.cpp
+++ src/utils.cpp
@@ -16,3 +16,35 @@ std::vector<char> utils::readFile(const std::string& filename) {
 
     return buffer;
 }
+
+void utils::loadModel(const char* path)
+{
+    tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+    std::string warn, err;
+
+    if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, path)) {
+        throw std::runtime_error(warn + err);
+    }
+
+    for (const auto& shape : shapes) {
+        for (const auto& index : shape.mesh.indices) {
+            Vertex vertex{};
+            vertex.pos = {
+                attrib.vertices[3 * index.vertex_index + 0],
+                attrib.vertices[3 * index.vertex_index + 1],
+                attrib.vertices[3 * index.vertex_index + 2]
+            };
+
+            vertex.color = {
+                attrib.normals[3 * index.vertex_index + 0],
+                attrib.normals[3 * index.vertex_index + 1],
+                attrib.normals[3 * index.vertex_index + 2]
+            };
+
+            // vertices.push_back(vertex);
+            // indices.push_back(indices.size());
+        }
+    }
+}
